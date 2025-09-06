4a3806c5de5f07055cb61e99e046aa15d209bc0d
naymaaminnishy <naymaaminnishy06@gmail.com>
2025-09-07 03:05:30 +0600

    Fix

--- frontend/src/components/dashboardLayout.jsx
+++ frontend/src/components/dashboardLayout.jsx
@@ -1,26 +1,14 @@
- import React, { useState } from "react";
+import React, { useState } from "react";
 import "./dashboardLayout.css";
 import logo from "../assets/logo.png";
 import { Menu } from "lucide-react";
-import { signOut } from "firebase/auth";
-import { auth } from "../firebase";
 
 export default function DashboardLayout({ sidebarItems, children, username }) {
   const [isCollapsed, setIsCollapsed] = useState(false);
-  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const getFirstName = (name) => {
     if (!name) return "User";
     return name.split(" ")[0];
-  };
-
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      window.location.reload();
-    } catch (err) {
-      console.error("Logout failed:", err);
-    }
   };
 
   return (
@@ -46,23 +34,8 @@ export default function DashboardLayout({ sidebarItems, children, username }) {
         <header className="topbar">
           <img src={logo} alt="SmartLearn Logo" className="logo" />
           <h1>Classroom</h1>
-
-          <div className="profile-dropdown">
-            <div
-              className="profile-name"
-              onClick={() => setDropdownOpen(!dropdownOpen)}
-            >
-              {getFirstName(username)} ⬇
-            </div>
-
-            {dropdownOpen && (
-              <div className="dropdown-menu">
-                <button onClick={handleLogout}>Logout</button>
-              </div>
-            )}
-          </div>
+          <div className="profile-name">{getFirstName(username)}</div>
         </header>
-
         <div className="grid">{children}</div>
       </main>
     </div>
